f579fbeef7208d63809c1c4ba2ac1bdac9169e00
Farouk Jabberi <jabberi.contact@gmail.com>
2020-07-07 17:21:06 +0200

    flow control :"while"loop

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -9,13 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // for on a number range  + step
-        for (i in 1..5 step 2) {
-            println(i)
-        }
-
-        // for on a number range + step
-        for (i in 5 downTo 1 step 2) {
+        // for on a number range
+        for (i in 0..9) {
             println(i)
         }
 
@@ -24,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         // for loop
         for (name in names) {
+            //do something with item
             println(name)
         }
 
@@ -32,6 +28,15 @@ class MainActivity : AppCompatActivity() {
             println("${name} est à l'index ${index}")
         }
 
+        // while
+        var age: Int = 0
+        while(age < 5) {
+            println("Tu as ${age} ans, c'est trop jeune reviens l'année prochaine!")
+            // do some code during the year
+            age++
+        }
+        println("OK tu as maintenant ${age} ans, tu peux jouer au basket!")
+
         // for + continue
         for(name in names) {
             if(name == "Jane") {
@@ -39,6 +44,16 @@ class MainActivity : AppCompatActivity() {
                 continue
             }
             println("${name} est présent")
+        }
+
+        // for + break
+        val captainName: String = "Jane"
+        for(name in names) {
+            if(name == captainName) {
+                println("Nous avons trouvé le capitaine ${name}")
+                break
+            }
+            println("${name} n'est pas capitaine")
         }
     }
 }
