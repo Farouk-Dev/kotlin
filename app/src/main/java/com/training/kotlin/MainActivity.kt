04e08fd736692be2e1943d46fdf21d10837d0d0b
Farouk Jabberi <jabberi.contact@gmail.com>
2020-07-07 13:10:59 +0200

    Variables

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -8,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var age: Int = 5
+        var name: String = "Bob"
+        var height: Float = 1.60F
+
+        println("${name} a ${age} ans")
+
+        age = 10
+        println("${name} a ${age} ans")
+
+        age *= 2
+        println("${name} a ${age} ans")
     }
 }
