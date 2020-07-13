770d9865d59309704eef621ca718124387d5749c
Farouk Jabberi <jabberi.contact@gmail.com>
2020-07-13 13:05:55 +0200

    explanation:POO

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -3,10 +3,29 @@ package com.training.kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+class Car(val wheelsCount: Int = 4) {
+
+    fun honk() {
+        println("Pouet!")
+    }
+
+    fun honkForWheels() {
+        println("Honking for wheels")
+        for(i in 0..this.wheelsCount) {
+            honk()
+        }
+    }
+}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val car = Car()
+        println("Nombre de roues: ${car.wheelsCount}")
+
+        car.honk()
+        car.honkForWheels()
     }
 }
