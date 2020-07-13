444005dc1ac3be38387e4c26333195a46b614447
Farouk Jabberi <jabberi.contact@gmail.com>
2020-07-13 13:59:49 +0200

    Encapsulation

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -3,27 +3,28 @@ package com.training.kotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-open class Vehicle(val wheelCount: Int) {
-    fun showWheels() {
-        println("Le vehicule a ${this.wheelCount} roues")
-    }
-     // methode ouverte à la surcharge
-    open fun honk() {
-        println("Pas de klaxon")
+open class Account {
+    protected var balance: Int = 0
+
+    open fun deposit(value: Int) {
+        balance += value
+        val prefix = if (value >= 0) "+" else ""
+        println("${prefix}${value} € sur le compte, nouveau solde : ${balance} €")
     }
 }
 
-class Car : Vehicle(4) {
-    // methode surchargée
-    override fun honk() {
-        println("Pouet!")
-    }
-}
+class PositiveAccount : Account() {
 
-class Motorcycle : Vehicle(2) {
-    // methode surchargée
-    override fun honk() {
-        println("Tsouin!")
+    override fun deposit(value: Int) {
+        if (!isValidDeposit(value)) {
+            println("Opération négative interdite sur un PositiveAccount")
+            return
+        }
+        super.deposit(value)
+    }
+
+    private fun isValidDeposit(value: Int) : Boolean {
+        return balance + value >= 0
     }
 }
 
@@ -32,12 +33,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var vehicle: Vehicle = Car()
-        vehicle.showWheels()
-        vehicle.honk()
+        println("Compte normal")
+        val account = Account()
+        account.deposit(50)
 
-        vehicle = Motorcycle()
-        vehicle.showWheels()
-        vehicle.honk()
+        println("Compte positif")
+        val positiveAccount = PositiveAccount()
+        positiveAccount.deposit(42)
+        positiveAccount.deposit(-100)
+        positiveAccount.deposit(-10)
     }
 }
