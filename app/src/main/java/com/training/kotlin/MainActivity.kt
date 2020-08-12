a473790bc0a7d3399ccb309b012cce4167ceb91f
Farouk Jabberi <jabberi.contact@gmail.com>
2020-08-12 17:46:29 +0200

    Cohabitation with java

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -1,25 +1,16 @@
 package com.training.kotlin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-abstract class Vehicle(val wheelsCount: Int) {
-
-    companion object Factory {
-        fun createCar() : Car = Car(4)
-        fun createMotorcycle() : Motorcycle = Motorcycle(2)
-    }
-}
-
-class Car(wheelsCount: Int) : Vehicle(wheelsCount)
-
-class Motorcycle(wheelsCount: Int) : Vehicle(wheelsCount)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val car = Vehicle.createCar()
-        val motorcycle = Vehicle.Factory.createMotorcycle()
+        val bob = User()
+        bob.age = 5
+        Log.i("MainActivity", "Adresse de bob : ${bob.address}")
     }
 }
