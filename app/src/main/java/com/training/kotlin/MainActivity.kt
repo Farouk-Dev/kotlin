a3249dbabd7c5c34036c93ba56f4db6f79d13633
Farouk Jabberi <jabberi.contact@gmail.com>
2020-08-13 21:32:31 +0200

    Lazy init

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -1,46 +1,29 @@
 package com.training.kotlin
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val helloWorldLazy: TextView by lazy {
+        println("lazy init")
+        findViewById(R.id.hello_world) as TextView
+    }
 
+    lateinit var helloWorldLate: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // declaration of a mutable map
-        val languages = mutableMapOf(
-            "Kotlin" to "Un super langage",
-            "Java" to "Un langage qui a fait son temps")
+        // crash if called before setContentView
+        println(helloWorldLazy.text)
+        helloWorldLate = findViewById(R.id.hello_world) as TextView
+    }
+    override fun onResume() {
+        super.onResume()
+        println(helloWorldLazy.text)
 
-        languages.put("C++", "Une des origines du Java")
-        println(languages)
-
-        println("Valeur de la clé Kotlin: ${languages["Kotlin"]}")
-
-        if (!languages.contains("Python")) {
-            println("Il manque le Python!")
-        }
-
-        println("foreach sur les Entry")
-        for (entry in languages.entries) {
-            println("${entry.key} => ${entry.value}")
-        }
-
-        println("foreach multi-declaration")
-        for ((key, value) in languages) {
-            println("$key => $value")
-        }
-
-        println("foreach sur les clés")
-        for (language in languages.keys) {
-            println("$language")
-        }
-
-        val nonCppLanguages = languages
-            .filterNot { it.key == "C++" }
-            .mapValues { it.value.toUpperCase() }
-        println(nonCppLanguages)
+        // crash if late init has not been initialized
+        println(helloWorldLate.text)
     }
 }
