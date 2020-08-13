a4666114b50561684e72d0826d4fc267ca6c8688
Farouk Jabberi <jabberi.contact@gmail.com>
2020-08-13 21:59:48 +0200

    Const

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -4,26 +4,20 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+const val VERSION: Int = 42
+const val NAME = "Bob"
+
 class MainActivity : AppCompatActivity() {
-    val helloWorldLazy: TextView by lazy {
-        println("lazy init")
-        findViewById(R.id.hello_world) as TextView
+
+    companion object {
+        const val TEST = "it works!"
     }
 
-    lateinit var helloWorldLate: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // crash if called before setContentView
-        println(helloWorldLazy.text)
-        helloWorldLate = findViewById(R.id.hello_world) as TextView
-    }
-    override fun onResume() {
-        super.onResume()
-        println(helloWorldLazy.text)
-
-        // crash if late init has not been initialized
-        println(helloWorldLate.text)
+        val localVersion = VERSION
+        val localTest = MainActivity.TEST
     }
 }
