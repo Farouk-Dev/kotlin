abd08558f7cda790537ab0ef46e2d22db86dfaf7
Farouk Jabberi <jabberi.contact@gmail.com>
2020-08-14 14:58:36 +0200

    Function extensions : apply

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -4,29 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 
+fun createDirectory(path: String) : File {
+    val file = File(path)
+    file.mkdirs()
+    return file
+}
+
+data class User(var name: String = "", var age: Int = 0)
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // file does not exist outside let { } scope
-        File("config").let { file ->
+        val longUser = User()
+        longUser.name = "LongBob"
+        longUser.age = 10
+        println("$longUser")
 
-            if (file.exists()) {
-                println("$file existe!")
-            } else {
-                file.createNewFile()
-                println("$file fichier créé")
-            }
+        val shortUser = User().apply {
+            name = "ShortBob"
+            age = 5
         }
+        println("$shortUser")
 
-        val file: File? = null
-        //file = File("nullable_file")
-
-        // let block is executed only if file != null
-        file?.let {
-            println("$it n'est pas null")
-        }
+        val longHome = createDirectory("home")
+        val shortHome = File("home").apply { mkdirs() }
     }
 }
