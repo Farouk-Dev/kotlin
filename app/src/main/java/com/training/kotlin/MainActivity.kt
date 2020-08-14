b5def67b62d799d9de262c448a37106a24bd12ca
Farouk Jabberi <jabberi.contact@gmail.com>
2020-08-14 13:53:49 +0200

    Function extensions : let

--- app/src/main/java/com/training/kotlin/MainActivity.kt
+++ app/src/main/java/com/training/kotlin/MainActivity.kt
@@ -1,42 +1,32 @@
 package com.training.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import training.androidkotlin.helloworld.utils.isVisible
-import training.androidkotlin.helloworld.utils.toast
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
 
-data class User(val name: String, val age: Int) {
-
-    companion object {
-        fun createBob() = User("Bob", 10)
-    }
-}
-
-// add extension function to existing class
-fun User.canPlayBasketBall() = this.age >= 10
-
-// add extension function to Companion object
-fun User.Companion.createBobette() = User("Bobette", 4)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloWorldView = findViewById(R.id.helloWorld) as TextView
+        // file does not exist outside let { } scope
+        File("config").let { file ->
 
-        // extend from the View class
-        if (helloWorldView.isVisible()) {
-            // extend from the Context (Activity == Context)
-            toast("Hello World!")
+            if (file.exists()) {
+                println("$file existe!")
+            } else {
+                file.createNewFile()
+                println("$file fichier créé")
+            }
         }
 
-        val bob = User.createBob()
-        println("${bob.name} peut jouer au basket-ball? ${bob.canPlayBasketBall()}")
+        val file: File? = null
+        //file = File("nullable_file")
 
-        val bobette = User.createBobette()
-        println("${bobette.name} peut jouer au basket-ball? ${bobette.canPlayBasketBall()}")
-
+        // let block is executed only if file != null
+        file?.let {
+            println("$it n'est pas null")
+        }
     }
 }
